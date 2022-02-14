253fbc21419927555e7e73d29f2bb46350efa9d2
Tomo Glozinic <tglozinic@dspace.hr>
2022-02-14 11:52:14 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelMinutes = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelStotinka = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUnosMinuta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.buttonResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMinutes
+            // 
+            this.labelMinutes.AutoSize = true;
+            this.labelMinutes.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelMinutes.Location = new System.Drawing.Point(129, 107);
+            this.labelMinutes.Name = "labelMinutes";
+            this.labelMinutes.Size = new System.Drawing.Size(67, 54);
+            this.labelMinutes.TabIndex = 0;
+            this.labelMinutes.Text = "00";
+            this.labelMinutes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelSeconds.Location = new System.Drawing.Point(217, 107);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(67, 54);
+            this.labelSeconds.TabIndex = 1;
+            this.labelSeconds.Text = "00";
+            this.labelSeconds.Click += new System.EventHandler(this.labelSeconds_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(190, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ":";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(46, 242);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(119, 42);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(191, 242);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(119, 42);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "PAUSE";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(336, 242);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(119, 42);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // labelStotinka
+            // 
+            this.labelStotinka.AutoSize = true;
+            this.labelStotinka.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelStotinka.Location = new System.Drawing.Point(301, 107);
+            this.labelStotinka.Name = "labelStotinka";
+            this.labelStotinka.Size = new System.Drawing.Size(67, 54);
+            this.labelStotinka.TabIndex = 6;
+            this.labelStotinka.Text = "00";
+            this.labelStotinka.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(278, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 54);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ".";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // textBoxUnosMinuta
+            // 
+            this.textBoxUnosMinuta.Location = new System.Drawing.Point(46, 49);
+            this.textBoxUnosMinuta.Name = "textBoxUnosMinuta";
+            this.textBoxUnosMinuta.Size = new System.Drawing.Size(41, 23);
+            this.textBoxUnosMinuta.TabIndex = 8;
+            this.textBoxUnosMinuta.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Unesi minute odbrojavanja pa klikni START";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 600;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Location = new System.Drawing.Point(191, 291);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(119, 39);
+            this.buttonResume.TabIndex = 10;
+            this.buttonResume.Text = "RESUME";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            this.buttonResume.Click += new System.EventHandler(this.buttonResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 342);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUnosMinuta);
+            this.Controls.Add(this.labelStotinka);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.labelMinutes);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMinutes;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelStotinka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUnosMinuta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button buttonResume;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        int seconds, stotinka;
+        bool active;
+        int unosMin;
+        bool valid;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //izvršava se kod pritiska na gumb "PAUSE"
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            active = false;
+        }
+        //izvršava se kod pritiska na gumb "RESUME"
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            active = true;
+        }
+
+        //izvršava se kod pritiska na gumb "RESET"
+        // gašenje timera i pozivanje fukncije Reset()
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            active = false;
+            Reset();
+        }
+
+
+        //izvršava se kod pritiska na gumb "START"
+            // podešvanje početnih vrijednosti na štoperici
+            // čitanje vrijednosti s labele i pretvaranje u oblik integer
+            // pokretanje timera
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            unosMin = Int32.Parse(textBoxUnosMinuta.Text) - 1;
+            seconds = 59;
+            stotinka = 100;
+            active = true;
+
+        }
+
+
+        //funkcija timera
+        //brojanje vremena unatrag od zadane vrijednosti
+        //blinkanje labele zadnjih 30 sekundi i pozivanje novog timera 
+        //reset funkcije kod isteka vremena
+        //pozivanje DisplayTime() funkcije
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (active)
+            {
+                stotinka--;
+                if (stotinka <= 0)
+                {
+                    seconds--;
+                    stotinka = 100;
+                    if (seconds <= 0)
+                    {
+                        unosMin--;
+                        seconds = 59; 
+                    }
+                }
+
+                if (unosMin < 0)
+                {
+                    Kraj();
+                }
+            }
+            DisplayTime();
+        }
+
+
+        //Funkcija za prikaz vremena na labelama
+        // pretvara integer vrijednosti u string i prikazuje u zadanom formatu
+        private void DisplayTime()
+        {
+            labelStotinka.Text = stotinka.ToString("00");
+            labelSeconds.Text = seconds.ToString("00");
+            labelMinutes.Text = unosMin.ToString("00");
+        }
+
+        //Funkcija za podešavanje vremena na početne vrijednosti i gašenje timera
+        private void Reset()
+        {
+            seconds = 0;
+            unosMin = 0;
+            stotinka = 0;
+            active = false;
+
+        }
+
+        //Funkcija za prikaz dijaloga, zvuka za istek vremena i izlazak iz aplikacije
+        private void Kraj()
+        {
+            Reset();
+            MessageBox.Show("Vrijeme je isteklo");
+            SystemSounds.Asterisk.Play();
+            System.Environment.Exit(1);
+
+        }
+
+        //Timer za blinkanje labela prikaza vremena zadnjih 30 sekundi i promijene boje u crveno
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (active)
+            {
+
+                if ((unosMin == 0) && (seconds < 31))
+                {
+                    labelMinutes.ForeColor = System.Drawing.Color.Red;
+                    labelSeconds.ForeColor = System.Drawing.Color.Red;
+                    labelStotinka.ForeColor = System.Drawing.Color.Red;
+                    label1.ForeColor = System.Drawing.Color.Red;
+                    label2.ForeColor = System.Drawing.Color.Red;
+
+                    labelMinutes.Visible = !labelMinutes.Visible;
+                    labelSeconds.Visible = !labelSeconds.Visible;
+                    labelStotinka.Visible = !labelStotinka.Visible;
+                    label1.Visible = !label1.Visible;
+                    label2.Visible = !label2.Visible;
+            }
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSeconds_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
